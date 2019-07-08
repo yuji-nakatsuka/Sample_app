4c72fc051c3bb53ee4e61d4bbe611f4b9f33ab63
yuji-nakatsuka <u.z.aka.dj0ma@gmail.com>
2019-07-08 12:37:32 +0000

    [Finish]4-7

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,9 +18,19 @@ class PostsController < ApplicationController
   	@posts = Post.all
   end
 
-    def show
+  def show
       @post = Post.find(params[:id])
-    end
+  end
+
+  def edit
+      @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
+  end
 
 	private
   	def post_params
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
 
   get 'posts/:id' => 'posts#show' , as: 'post'
   # .../posts/1や.../posts/3に該当する
+
+  get 'posts/:id/edit' => 'posts#edit',as: 'edit_post'
+
+  patch 'posts/:id' => 'posts#update' , as: 'update_post'
 end
